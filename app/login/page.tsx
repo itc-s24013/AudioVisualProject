bb0cbee7d8cde5f6e518a444633af92d50bdf540
s25003 <s25003@std.it-college.ac.jp>
2026-08-19 09:26:28 +0900

    Update PRODUCTIONなんとか

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,8 @@ import { createClient } from "@/lib/supabase/client";
 import Image from "next/image";
 import { useCallback, useEffect, useRef, useState } from "react";
 
-const PRODUCTION_ORIGIN = "https://audio-visual-project-chi.vercel.app";
+// const PRODUCTION_ORIGIN = "https://audio-visual-project-chi.vercel.app";
+const PRODUCTION_ORIGIN = "https://audio-visual-project-git-p5js-update-s24013s-projects.vercel.app";
 
 export default function LoginPage() {
   const [errorMessage, setErrorMessage] = useState("");
